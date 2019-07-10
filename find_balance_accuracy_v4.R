2071cd8ae692d206667418861bdada6defbae8a6
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-07-10 14:43:06 -0700

    Update find_balance_accuracy_v4.R

--- find_balance_accuracy_v4.R
+++ find_balance_accuracy_v4.R
@@ -13,7 +13,6 @@ for (i in 1:num_files) {
   #file_name <- file_name_list[i]
   file_name = file_name_list[(i)]
   if(length(grep("_stats_preds.RData", file_name ,fixed=TRUE)) == 1){
-  
     load(paste0("/Users/user/Desktop/BD2K_project/data/", file_name))
     stats = confM[[4]]
     bal_accs = stats[,11]
