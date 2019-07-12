f37cc330786526b4d841c56313d1cd1e5ee7c801
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-07-11 19:40:43 -0700

    Add files via upload

---
+++ find_balance_accuracy_v5.R
@@ -0,0 +1,29 @@
+## Gabe & Jackie
+# This script prints the average balance accuracy for each AKLIMATE output file,
+# finds the summary statistics, the standard deviation, the standard error, and plots them.
+
+# file_name_list: Takes all the files and lists them
+file_name_list = list.files(path = "/Users/jacquelynroger/Documents/research/RMI/gabe/data")
+# num_files: The number of files we have
+num_files = length(file_name_list)
+
+# Initialize empty list to store the aggregate of all of the balance accuracies in this cohort
+bal_accs_all = list()
+
+# LOOP A: This for loop will run through all the files we have 
+for (i in 1:num_files) {
+  file_name = file_name_list[(i)]
+  # LOOP B: This if statement makes sure we only go into the files that are specifically AKLIMATE output files
+  if(length(grep("_stats_preds.RData", file_name ,fixed=TRUE)) == 1){
+    load(paste0("/Users/jacquelynroger/Documents/research/RMI/gabe/data/", file_name))
+    stats = confM[[4]]
+    bal_accs = stats[,11]
+    bal_accs_all = c(bal_accs_all, bal_accs)
+  }
+}
+
+# This now contains all balance accuracies for the cohort
+bal_accs_all = unlist(bal_accs_all)
+
+# Make a boxplot of the balance accuracies in this cohort
+boxplot(bal_accs_all, horizontal = TRUE, main = "Balance accuracies across cohorts", xlab = "Balance accuracy", ylim = c(0, 1))
