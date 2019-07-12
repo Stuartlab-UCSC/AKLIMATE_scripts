239ce7f700c375638b0af4c7f83138a42199c1c1
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-07-11 20:03:33 -0700

    Update find_balance_accuracy_v5.R

--- find_balance_accuracy_v5.R
+++ find_balance_accuracy_v5.R
@@ -15,7 +15,7 @@ for (i in 1:num_files) {
   file_name = file_name_list[(i)]
   # LOOP B: This if statement makes sure we only go into the files that are specifically AKLIMATE output files
   if(length(grep("_stats_preds.RData", file_name ,fixed=TRUE)) == 1){
-    load(paste0("/Users/jacquelynroger/Documents/research/RMI/gabe/data/", file_name))
+    load(paste0("/scratch/for_gchavez/aklimate_results/thym/models/", file_name))
     stats = confM[[4]]
     bal_accs = stats[,11]
     bal_accs_all = c(bal_accs_all, bal_accs)
