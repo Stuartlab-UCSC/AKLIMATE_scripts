7e9735a014ff08331a09a8c3598c20577bfce81f
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-07-15 15:43:15 -0700

    Update find_balance_accuracy_v5.R

--- find_balance_accuracy_v5.R
+++ find_balance_accuracy_v5.R
@@ -11,13 +11,13 @@
 tumor_type_list = c("brca", "coadread", "lgggbm", "thym", "ucec")
 # LOOP A: This for loop will run through all the files we have 
 for(tt in tumor_type_list){
- filepath = paste("/scratch/for_gchavez/aklimate_results/",tt,"/models/",sep="")
- # file_name_list: Takes all the files and lists them
- file_name_list = list.files(path = filepath)
- # num_files: The number of files we have
- num_files = length(file_name_list)
- # Initialize empty list to store the aggregate of all of the balance accuracies in this cohort
- bal_accs_all = list()
+  filepath = paste("/scratch/for_gchavez/aklimate_results/",tt,"/models/",sep="")
+  # file_name_list: Takes all the files and lists them
+  file_name_list = list.files(path = filepath)
+  # num_files: The number of files we have
+  num_files = length(file_name_list)
+  # Initialize empty list to store the aggregate of all of the balance accuracies in this cohort
+  bal_accs_all = list()
   for (i in 1:num_files) {
     file_name = file_name_list[(i)]
     # LOOP B: This if statement makes sure we only go into the files that are specifically AKLIMATE output files
@@ -39,3 +39,9 @@ for(tt in tumor_type_list){
           , ylim = c(0, 1), ylab = "Cohorts", names = c("BRCA"))
   dev.off()
 }
+boxplot(bal_accs_all, main = "Balance accuracies across cohorts", at = c(length(tt)),
+names = c(tt), las = 2,
+col = c("orange","red", "blue", "green", "purple"),
+border = "brown", horizontal = TRUE, notch = TRUE, xlab = "Balance accuracy"
+, ylim = c(0, 1), ylab = "Cohorts"
+)
