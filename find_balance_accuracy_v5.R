05fbd5d91c81e2c431f83c9d866b5c35c87cf413
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-07-15 12:34:56 -0700

    Update find_balance_accuracy_v5.R

--- find_balance_accuracy_v5.R
+++ find_balance_accuracy_v5.R
@@ -17,13 +17,12 @@ num_files = length(file_name_list)
 bal_accs_all = list()
 #the types of cohorts we're comparing 
 tumor_type_list = c("brca", "coadread", "lgggbm", "thym", "ucec")
-# LOOP A: This for loop will run through all the COHORT's we have
+# LOOP A: This for loop will run through all the files we have 
 for(tt in tumor_type_list){
  paste("/scratch/for_gchavez/aklimate_results/",tt,"/models/",sep="")
-# LLOP B: This for loop will run through all the files we have 
   for (i in 1:num_files) {
     file_name = file_name_list[(i)]
-    # This if statement makes sure we only go into the files that are specifically AKLIMATE output files
+    # LOOP B: This if statement makes sure we only go into the files that are specifically AKLIMATE output files
     if(length(grep("_stats_preds.RData", file_name ,fixed=TRUE)) == 1){
       load(paste0("/scratch/for_gchavez/aklimate_results/thym/models/", file_name))
       stats = confM[[4]]
@@ -31,11 +30,10 @@ for(tt in tumor_type_list){
       bal_accs_all = c(bal_accs_all, bal_accs)
     }
   }
+  # This now contains all balance accuracies for the cohort
+  #unlist: flattens a list and turns in into sorta a vector
+  bal_accs_all = unlist(bal_accs_all)
+  
+  # Make a boxplot of the balance accuracies in this cohort
+  boxplot(bal_accs_all, horizontal = TRUE, main = "Balance accuracies across cohorts", xlab = "Balance accuracy", ylim = c(0, 1), ylab = "Cohorts", names = c("BRCA"))
 }
-
-# This now contains all balance accuracies for the cohort
-#unlist: flattens a list and turns in into sorta a vector
-bal_accs_all = unlist(bal_accs_all)
-
-# Make a boxplot of the balance accuracies in this cohort
-boxplot(bal_accs_all, horizontal = TRUE, main = "Balance accuracies across cohorts", xlab = "Balance accuracy", ylim = c(0, 1), ylab = "Cohorts", names = c("BRCA"))
