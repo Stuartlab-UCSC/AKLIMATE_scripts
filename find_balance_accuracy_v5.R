13bc4901b8c85c9378637398dee937899b447077
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-07-15 14:07:45 -0700

    Update find_balance_accuracy_v5.R

--- find_balance_accuracy_v5.R
+++ find_balance_accuracy_v5.R
@@ -2,29 +2,28 @@
 # This script prints the average balance accuracy for each AKLIMATE output file,
 # finds the summary statistics, the standard deviation, the standard error, and plots them.
 
+# file_name_list paths:
+#in ucsc terminal: /scratch/for_gchavez/aklimate_results/thym/models/
+#in gabes computer: /Users/user/Desktop/BD2K_project/data/
+#in jackies computer: /Users/jacquelynroger/Documents/research/RMI/gabe/data/
 
-
-
-# file_name_list: Takes all the files and lists them
-#path in ucsc terminal: /scratch/for_gchavez/aklimate_results/thym/models/
-#path in gabes computer: /Users/user/Desktop/BD2K_project/data/
-#path in jackies computer: /Users/jacquelynroger/Documents/research/RMI/gabe/data
-file_name_list = list.files(path = "/scratch/for_gchavez/aklimate_results/thym/models/")
-# num_files: The number of files we have
-num_files = length(file_name_list)
-
-# Initialize empty list to store the aggregate of all of the balance accuracies in this cohort
-bal_accs_all = list()
-#the types of cohorts we're comparing 
+#tumor_type_list: the types of cohorts we're comparing 
 tumor_type_list = c("brca", "coadread", "lgggbm", "thym", "ucec")
 # LOOP A: This for loop will run through all the files we have 
 for(tt in tumor_type_list){
- paste("/scratch/for_gchavez/aklimate_results/",tt,"/models/",sep="")
+ filepath = paste("/scratch/for_gchavez/aklimate_results/",tt,"/models/",sep="")
+ # file_name_list: Takes all the files and lists them
+ file_name_list = list.files(path = filepath)
+ # num_files: The number of files we have
+ num_files = length(file_name_list)
+ # Initialize empty list to store the aggregate of all of the balance accuracies in this cohort
+ bal_accs_all = list()
   for (i in 1:num_files) {
     file_name = file_name_list[(i)]
     # LOOP B: This if statement makes sure we only go into the files that are specifically AKLIMATE output files
     if(length(grep("_stats_preds.RData", file_name ,fixed=TRUE)) == 1){
-      load(paste0("/scratch/for_gchavez/aklimate_results/thym/models/", file_name))
+      load(paste0("/scratch/for_gchavez/aklimate_results/",tt,"/models/",sep="", file_name))
+      # Initialize empty list to store the aggregate of all of the balance accuracies in this cohort
       stats = confM[[4]]
       bal_accs = stats[,11]
       bal_accs_all = c(bal_accs_all, bal_accs)
