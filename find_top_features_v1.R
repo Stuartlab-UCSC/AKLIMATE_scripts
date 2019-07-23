131e6d0b9e94dece56ecf504234b50324a04d2ce
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-07-23 14:17:00 -0700

    Update find_top_features_v1.R

--- find_top_features_v1.R
+++ find_top_features_v1.R
@@ -1,53 +1,85 @@
 ###(Cohort, Assays, Readily, Identified, Now, Algorithm)
-#Want to find the most important features of each cancer cohort
-
-#precondition: x must be less that or equal to the numba of features in the first set.
-#set numba of top features (x)
-x = 10
-#helps so we dont have to write jk... over agian
-load("/scratch/for_gchavez/aklimate_results/brca/models/R1:F1_junkle_final_model.RData")
-list_of_sets = jklm$rf.stats$importance
-#takes the first set in our list of sets (set = cohorst)
-first_set = list_of_sets[[1]]
-#finds the first name of the first set
-first_set_name = names(list_of_sets)[1]
-#grabs the first 10 rows in our set
-first_x_features_and_importances = first_set[1:x]
-#initializes an empty vector
-first_x_features = c()
-first_x_importances = c()
-#this loop, 
-for(i in 1:x){
-  #combines the lists
-  first_x_features = c(first_x_features, names(first_x_features_and_importances)[i])
-  #same as above for the features
-  first_x_importances = c(first_x_importances, first_x_features_and_importances[[i]])                    
-}
-#tells where to put what in each coloumn
-top_features = cbind(feature = first_x_features,
-                     importance = first_x_importances,
-                     set = first_set_name)
-#when running for real, replace 5 with length(list_of_sets)
-for(j in 2:5){
-  current_set = list_of_sets[[j]]
-  #can improve by making into a function!
-  current_set_name = names(list_of_sets)[j]
-  current_x_features_and_importances = current_set[1:x]
-  current_x_features = c()
-  current_x_importances = c()
-  for(k in 1:x){
-    current_x_features = c(current_x_features, names(current_x_features_and_importances)[k])
-    current_x_importances = c(current_x_importances, current_x_features_and_importances[[k]])                    
+#by Gabe & verenna
+#/scratch/for_gchavez/aklimate_results/brca/models/
+#this is where our junkle data is at
+list_of_sets = c(jklm$rf.stats$importance)
+#the five cohorts we're looking at
+tumor_type_list = c("brca", "coadread", "lgggbm", "thym", "ucec")
+#for later use
+df_cohort = c()
+# LOOP A: This for loop will run through all the server cohort files we have
+for (i in 1:length(tumor_type_list)) {
+  #an abriviation that loops through all five server cohorts
+  tt = tumor_type_list[i]
+  #this is where we will eventually stack all the junkle data together
+  Junkle_data_list = c()
+  #the path to th files
+  filepath = paste("/Users/user/Desktop/BD2K_project/data/")
+  # file_name_list: Takes all the files and lists them
+  file_name_list = list.files(path = filepath)
+  # num_files: The number of files we have
+  num_files = length(file_name_list)
+  #this for loop
+  for (j in 1:num_files) {
+    file_name = file_name_list[(j)]
+    if (length(grep("_junkle_final_model.RData", file_name , fixed = TRUE)) == 1) {
+      load(paste0("/Users/user/Desktop/BD2K_project/data/", file_name))
+      list_of_sets = c(jklm$rf.stats$importance)
+      #put the data files in a list called:junkle_data_list
+      Junkle_data_list = c(Junkle_data_list, Junkle_data_list[j])
+    }
   }
-  current_top_features = cbind(feature = current_x_features,
-                               importance = current_x_importances,
-                               set = current_set_name)
-  top_features = rbind(top_features, current_top_features)
-  
-  top_features = as.data.frame(top_features)
-  top_features$importance = as.numeric(as.character(top_features$importance))
- attach(top_features, warn.conflicts = FALSE)
-  top_features = top_features[order(-importance),]
-  top_features = top_features[1:x,]
+  x = 10
+  #takes the first set in our list of sets (set = cohorst)
+  first_set = list_of_sets[[1]]
+  #finds the first name of the first set
+  first_set_name = names(list_of_sets)[1]
+  #grabs the first 10 rows in our set
+  first_x_features_and_importances = first_set[1:x]
+  #initializes an empty vector
+  first_x_features = c()
+  first_x_importances = c()
+  #this loop,
+  for (i in 1:x) {
+    #combines the lists
+    first_x_features = c(first_x_features,
+                         names(first_x_features_and_importances)[i])
+    #same as above for the features
+    first_x_importances = c(first_x_importances, first_x_features_and_importances[[i]])
+  }
+  #tells where to put what in each coloumn
+  top_features = cbind(feature = first_x_features,
+                       importance = first_x_importances,
+                       set = first_set_name,
+                       cross_validation = current_CV)
+  #when running for real, replace 5 with length(list_of_sets)
+  for (j in 2:5) {
+    current_set = list_of_sets[[j]]
+    #can improve by making into a function!
+    current_set_name = names(list_of_sets)[j]
+    current_x_features_and_importances = current_set[1:x]
+    current_x_features = c()
+    current_x_importances = c()
+    current_CV = c()
+    for (k in 1:x) {
+      current_x_features = c(current_x_features,
+                             names(current_x_features_and_importances)[k])
+      current_x_importances = c(current_x_importances,
+                                current_x_features_and_importances[[k]])
+      current_CV = c(current_CV, ceiling(j/10452))
+    }
+    current_top_features = cbind(feature = current_x_features,
+                                 importance = current_x_importances,
+                                 set = current_set_name,
+                                 cross_validation = current_CV)
+    top_features = rbind(top_features, current_top_features)
+    
+    top_features = as.data.frame(top_features)
+    top_features$importance = as.numeric(as.character(top_features$importance))
+    attach(top_features, warn.conflicts = FALSE)
+    top_features = top_features[order(-importance),]
+    top_features = top_features[1:x,]
+  }
 }
 print(top_features)
+plot(top_features$importance)
