a822ea1e21cf57b8e1d1b9f39ef131a1afe307d0
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-07-24 16:47:30 -0700

    Add files via upload

---
+++ Chris_plots_sickle_v1.R
@@ -0,0 +1,11 @@
+# Stacked Bar Plot with Colors and Legend
+counts <- table(mtcars$vs, mtcars$gear)
+barplot(counts, main="Car Distribution by Gears and VS",
+        xlab="Number of Gears", col=c("darkblue","red"),
+        legend = rownames(counts))
+
+# Simple Scatterplot
+attach(mtcars)
+plot(wt, mpg, main="Scatterplot Example", 
+     xlab="Car Weight ", ylab="Miles Per Gallon ", pch=19)
+
