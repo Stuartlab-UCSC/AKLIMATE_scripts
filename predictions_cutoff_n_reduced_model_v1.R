3655113e42077863f53ed67a607a22a005835095
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-08-06 13:10:48 -0700

    Add files via upload

---
+++ predictions_cutoff_n_reduced_model_v1.R
@@ -0,0 +1,28 @@
+#Set x to cutoff number
+x = 50
+#tumor_type_list: the types of cohorts we're comparing
+tumor_type_list = c("brca", "coadread", "lgggbm", "thym", "ucec")
+for (j in 1:length(tumor_type_list)) {
+  tt = tumor_type_list[j]
+  #prediction class
+  prediction_class_tt = c()
+  filepath = paste("/Users/user/Desktop/BD2K_project/data/")
+  # file_name_list: Takes all the files and lists them
+  file_name_list = list.files(path = filepath)
+  # num_files: The number of files we have
+  num_files = length(file_name_list)
+  # Initialize empty list to store the aggregate of all of the balance accuracies in this cohort
+  bal_accs_all = c()
+  for (i in 1:num_files) {
+    file_name = file_name_list[(i)]
+    # LOOP B: This if statement makes sure we only go into the files that are specifically AKLIMATE output files
+    if (length(grep("_cutoff_",50,"_rf_reduced_model_predictions", file_name , fixed = TRUE)) == 1) {
+      load(paste0("/Users/user/Desktop/BD2K_project/data/", file_name))
+      # Initialize empty list to store the aggregate of all of the predictions in this cohort
+      pedictions = rf.preds
+    }
+    for (k in 1:length(rf,preds[,1]) {
+      prediction_class_tt = c(max(rf.preds[1,k], prediction_class)
+    }
+  }
+}
