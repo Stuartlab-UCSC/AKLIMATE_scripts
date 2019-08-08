bf1b67fef0fa32bca3688ae268905fbc91e166d1
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-08-07 21:09:45 -0700

    Update predictions_cutoff_n_reduced_model_v1.R

--- predictions_cutoff_n_reduced_model_v1.R
+++ predictions_cutoff_n_reduced_model_v1.R
@@ -1,3 +1,4 @@
+
 #Set x to cutoff number
 x = 50
 #tumor_type_list: the types of cohorts we're comparing
@@ -5,7 +6,6 @@ tumor_type_list = c("brca", "coadread", "lgggbm", "thym", "ucec")
 for (j in 1:length(tumor_type_list)){
   tt = tumor_type_list[j]
   #prediction class
-  df.prediction_class_tt = c()
   filepath = paste("/scratch/for_gchavez/aklimate_results/brca/models/")
   # file_name_list: Takes all the files and lists them
   file_name_list = list.files(path = filepath)
@@ -13,16 +13,15 @@ for (j in 1:length(tumor_type_list)){
   num_files = length(file_name_list)
   # Initialize empty list to store the aggregate of all of the balance accuracies in this cohort
   bal_accs_all = c()
-  for (i in 1:num_files){
-    file_name = file_name_list[(i)]
-    # LOOP B: This if statement makes sure we only go into the files that are specifically AKLIMATE output files
-    if (length(grep("_cutoff_",50,"_rf_reduced_model_predictions", file_name , fixed = TRUE)) == 1) {
-      load(paste0("/scratch/for_gchavez/aklimate_results/brca/models/", file_name))
-      # Initialize empty list to store the aggregate of all of the predictions in this cohort
-      pedictions = rf.preds
-    }
-    for (k in 1:length(rf.preds[,1])){
-      df.prediction_class_tt = c(rownames(rf.preds)[k],max(rf.preds[k,], prediction_class_tt))
+    for(r in 1:5){
+      for(f in 1:5){
+        #load(paste0("/scratch/for_gchavez/aklimate_results/brca/models/R",r,":F",f,"_cutoff_"
+                    #,x,"_rf_reduced_model_predictions.RData"))
+        load(paste0("/scratch/for_gchavez/aklimate_results/brca/models/"))
+        pedictions = rf.preds
+        for (k in 1:length(rf.preds[,1])){
+          df.prediction_class = c(df.prediction_class, max(rf.preds[k,]))
+        }
+      }
     }
   }
-}  
