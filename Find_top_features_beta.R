860480ffcb17a7aa5668d7b678e5374ff73a604c
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-08-12 12:19:49 -0700

    Update Find_top_features_beta.R

--- Find_top_features_beta.R
+++ Find_top_features_beta.R
@@ -15,13 +15,12 @@ for (i in 1:length(tumor_type_list)) {
   #this is where we will eventually stack all the junkle data together
   Junkle_data_list = c()
   #the path to th files
-  filepath = paste("/Users/user/Desktop/BD2K_project/data/")
-  # file_name_list: Takes all the files and lists them
+  filepath = paste("/scratch/for_gchavez_old/aklimate_results/",tt,"models")
+   # file_name_list: Takes all the files and lists them
   file_name_list = list.files(path = filepath)
   # num_files: The number of files we have
   num_files = length(file_name_list)
-  
-  feature_df = c()
+  feature_table = c()
   #this for loop
   for (j in 1:num_files) {
     file_name = file_name_list[(j)]
@@ -45,47 +44,21 @@ for (i in 1:length(tumor_type_list)) {
       # add column with the cross validation fold
       
       
-      feature_table = rbind(feature_df, list_of_sets)
+      feature_table = rbind(feature_table, list_of_sets)
     }
   }
   print("big list finished")
   x = 10
   #takes the first set in our list of sets (set = cohorst)
-  feature_table = c(list_of_sets[[1]], list_of_sets[[2]])
+  #feature_table = c(list_of_sets[[1]], list_of_sets[[2]])
   #grabs the first 10 rows in our set
-  first_x_features_and_importances = df.first_set[1:x]
+  feature_table[order(feature_table$importance,decreasing = TRUE),]
+  
+  top_features = feature_table[1:x,1]
+  top_importances = feature_table[1:x,2]
+  top_list = rbind(top_features, top_importances)
   #initializes an empty vector
-  first_x_features = c()
-  first_x_importances = c()
-  current_CV = c()
-  #this loop,
-  for (i in 1:x) {
-    #combines the lists
-    first_x_features = c(first_x_features,
-                         names(first_x_features_and_importances)[i])
-    #same as above for the features
-    first_x_importances = c(first_x_importances, first_x_features_and_importances[[i]])
-    for (j in 1:5) {
-      #this is for which iteration 
-      if (file_name == grepl("R",j,"_")) {
-        current_CV = j
-      }
-      #this is for cross validation fold
-      #if (file_name == grepl("_F",j)){
-      #  current_CV = j
-      #}
-      
-    }
-  }
-  #tells where to put what in each coloumn
-  top_features = cbind(
-    feature = first_x_features,
-    importance = first_x_importances,
-    cross_validation = current_CV
-  )
-  
-  
-  print(top_features)
+  print(top_list)
   # write.table(top_features, file = paste0("/scratch/for_gchavez/aklimate_results/",tt,"_top_features", x ,"-table.tsv")
   #,append = FALSE, quote = FALSE, sep = "\t", row.names = TRUE,
   #col.names = TRUE)
