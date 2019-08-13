59ae989deafee43c1e2e7e70ccb1ce4e8aba9ca9
Gabriel Michael Chavez <52179896+elcompagabe@users.noreply.github.com>
2019-08-13 15:02:53 -0700

    Update predictions_cutoff_n_reduced_model_v1.R

--- predictions_cutoff_n_reduced_model_v1.R
+++ predictions_cutoff_n_reduced_model_v1.R
@@ -1,27 +1,49 @@
-
 #Set x to cutoff number
-x = 50
+x = 10
 #tumor_type_list: the types of cohorts we're comparing
 tumor_type_list = c("brca", "coadread", "lgggbm", "thym", "ucec")
 for (j in 1:length(tumor_type_list)){
   tt = tumor_type_list[j]
+  df.prediction_class = c()
+  samples = c()
+  samples = rownames(rf.preds)
   #prediction class
-  filepath = paste("/scratch/for_gchavez/aklimate_results/brca/models/")
+  filepath = paste0("/scratch/for_gchavez/aklimate_results/",tt,"/models/")
   # file_name_list: Takes all the files and lists them
   file_name_list = list.files(path = filepath)
   # num_files: The number of files we have
   num_files = length(file_name_list)
   # Initialize empty list to store the aggregate of all of the balance accuracies in this cohort
   bal_accs_all = c()
-    for(r in 1:5){
-      for(f in 1:5){
-        #load(paste0("/scratch/for_gchavez/aklimate_results/brca/models/R",r,":F",f,"_cutoff_"
-                    #,x,"_rf_reduced_model_predictions.RData"))
-        load(paste0("/scratch/for_gchavez/aklimate_results/brca/models/"))
-        pedictions = rf.preds
-        for (k in 1:length(rf.preds[,1])){
-          df.prediction_class = c(df.prediction_class, max(rf.preds[k,]))
+  #for(r in 1:5){
+    #for(f in 1:5){
+      #load(paste0("/scratch/for_gchavez/aklimate_results/brca/models/R",r,":F",f,"_cutoff_"
+      #,x,"_rf_reduced_model_predictions.RData"))
+      #load(paste0("/Users/user/Desktop/BD2K_project/data/"))
+  print(tt)
+  print("prediction cutoff")
+      pedictions = rf.preds
+      for (k in 1:length(rf.preds[,1])){
+        #df.prediction_class = c(df.prediction_class, max(rf.preds[k,]))
+        if(max(rf.preds[k,]) == rf.preds[k,1]){
+          df.prediction_class = c(df.prediction_class, 1)
+        }
+        if(max(rf.preds[k,]) == rf.preds[k,2]){
+          df.prediction_class = c(df.prediction_class, 2)
+        }
+        if(max(rf.preds[k,]) == rf.preds[k,3]){
+          df.prediction_class = c(df.prediction_class, 3)
+        }
+        if(max(rf.preds[k,]) == rf.preds[k,4]){
+          df.prediction_class = c(df.prediction_class, 4)
+        }
+        if(max(rf.preds[k,]) == rf.preds[k,5]){
+          df.prediction_class = c(df.prediction_class, 5)
         }
       }
-    }
-  }
+      #SPC (Sample.Prediction.Class)
+      SPC = cbind(samples, df.prediction_class)
+    #}
+  #}
+  print(SPC)
+}
